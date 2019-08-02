991d2145fe00831236904d5e801607e50119a636
Viper Vit <vitolg1@gmail.com>
2019-08-01 21:48:23 -0400

    logging modified

--- viperlib/__init__.py
+++ viperlib/__init__.py
@@ -1,12 +1,6 @@
 import logging
 
-console = logging.StreamHandler()
-console.setLevel(logging.ERROR)
-
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
-
-logger.addHandler(console)
 
 from .src import misc as misc
 from .src import vtime as vtime
