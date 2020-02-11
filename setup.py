6d7ecb0785fb3be0575af8c96e08b8fbbb669f2e
Viper Vit <vitolg1@gmail.com>
2020-02-10 21:08:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author_email='vitolg1@gmail.com',
     license='Apache',
     description='General use library.',
-    version='0.45',
+    version='0.46',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "test*"])
 )
